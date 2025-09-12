a9f431075b3a9be2c6a32b840a7d959153901a88
Ola Kliuch <lifeproject42@gmail.com>
2025-09-12 18:44:09 +0200

    Add test for database

--- modules/common/database.py
+++ modules/common/database.py
@@ -11,3 +11,49 @@ class Database():
         self.cursor.execute(sqlite_select_Query)
         record = self.cursor.fetchall()
         print(f"Connected successfully. SQLite Database Version is: {record}")
+
+    def get_all_users(self):
+        query = "SELECT name, address, city FROM customers"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+    
+
+    def get_user_address_by_name(self, name):
+        query = f"SELECT address, city, postalCode, country FROM customers WHERE name = '{name}'"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+    
+    def update_product_qnt_by_id(self, product_id, qnt):
+        query = f"UPDATE products SET quantity = {qnt} WHERE id = {product_id}"
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    def select_product_qnt_by_id(self, product_id):
+        query = f"SELECT quantity FROM products WHERE id = {product_id}"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+    
+    def insert_product(self, product_id, name, description, qnt): #декларуємо метод з вхідними параметрами
+        query = f"INSERT OR REPLACE INTO products (id, name, description, quantity) \
+            VALUES ({product_id}, '{name}', '{description}', {qnt})"   #запит
+        self.cursor.execute(query) #виконання запиту
+        self.connection.commit()  #підтвердження того що база данних виконала ваш запит
+
+    def delete_product_by_id(self, product_id):
+        query = f"DELETE FROM products WHERE id = {product_id}"
+        self.cursor.execute(query)
+        self.connection.commit()\
+
+    def get_detailed_orders(self):
+        query = "SELECT orders.id, customers.name, products.name, \
+                products.description, orders.order_date \
+                FROM orders \
+                Join customers ON orders.customer_id = customers.id \
+                JOIN products ON orders.product_id = products.id"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+    
